5c6f0370e5dc1a1351002b0fe1581ddf20bd0c50
egor <yalosharik7@gmail.com>
2026-08-27 23:24:19 +0300

    chore: remove spike scaffolding, restore clean entry point

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,7 @@ import './shared/messages'
 export function main() {
   if (isServer()) {
     console.log('[SERVER] up')
-    // import('./server/server').then((m) => m.startServer())
   } else {
     console.log('[CLIENT] up')
-    // --- SPIKE C (throwaway) ---
-    void import('./client/spike-c-hud').then((m) => m.setupSpikeCHud())
-    void import('./client/spike-c').then((m) => m.startSpikeC())
   }
 }
